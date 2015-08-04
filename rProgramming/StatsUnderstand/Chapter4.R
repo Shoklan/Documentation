e4d8271a4316ba70288b445b498bf9f415b69f77
Shoklan <Shoklansenta@gmail.com>
2015-08-04 19:42:47 -0400

    Chapter 4 Base File
    Adding the skeleton file for Chapter 4

---
+++ rProgramming/StatsUnderstand/Chapter4.R
@@ -0,0 +1,10 @@
+# AUTHOR: COLLIN MITCHELL
+# DATE: 2015/8/2
+# PURPOSE:
+#        | This folder is for teaching myself Statistics using the textbook from my
+#        | Statistics 101 class from Monroe Community College.
+#        | This is for Chapter 4 of the book.
+####################################################################################
+
+# 4.1 Elementary Probability Theory
+# What is Probability?
