912ff1b7f00c373845c601d3b392dd8d09f810e9
Shoklan <Shoklansenta@gmail.com>
2015-08-20 11:29:56 -0400

    Chapter 5 Answers
    Answers

--- rProgramming/StatsUnderstand/Chapter5Answers.R
+++ rProgramming/StatsUnderstand/Chapter5Answers.R
@@ -6,3 +6,20 @@
 ####################################################################################
 
 # 5.1 Questions.
+# 1]
+#  a) Discrete
+#  b) Continuous
+#  c) Continuous 
+#  d) Discrete
+#  e) Continuous
+
+# 3]
+#  a) TRUE
+answer <- sum(.25, .6, .15)
+if (answer == 1) print(TRUE)
+
+#  b) FALSE
+answer <- sum(.25, .6, .2)
+if (answer == 1) print(TRUE)
+
+# 5] 
