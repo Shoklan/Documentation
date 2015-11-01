51e07f0f741e8b30a7a334d2263ba51be2177932
Collin Mitchell <Shoklansenta@gmail.com>
2015-11-01 06:32:17 -0500

    Working on Shiny Stuff
    Shiny Work

---
+++ rProgramming/Shiny/mixingShinyGoogleVis/ui.R
@@ -0,0 +1,11 @@
+# ui.R
+shinyUI(pageWithSidebar(
+  headerPanel("googleVis on Shiny"),
+  sidebarPanel(
+    selectInput("dataset", "Choose a dataset:",
+                choices = c("rock", "pressure", "cars"))
+  ),
+  mainPanel(
+    htmlOutput("view")
+  )
+))
