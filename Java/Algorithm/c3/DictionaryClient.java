4e26a7eb639b3cdf0988ec3081476892f40eb5e3
Collin Mitchell <Shoklansenta@gmail.com>
2015-12-05 07:49:22 -0500

    Working on Algorithms
    Derp

---
+++ Java/Algorithm/c3/DictionaryClient.java
@@ -0,0 +1,12 @@
+public static void main(String[] args){
+	ST<String, Integer> st;
+	st = new ST<String, Integer>();
+
+	for(int i=0; !StdIn.isEmpty(); i++){
+		String key = StdIn.readString();
+		st.put(key, i);
+	}
+
+	for(String s: st.keys())
+		StdOut.println(s + " " + st.gets(s));
+}
