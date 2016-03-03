e222cd7797f5d4939224e47b6b7c5387ab1fc69a
Collin Mitchell <Shoklansenta@gmail.com>
2016-03-03 06:43:43 -0500

    Starting Chapter 9
    Apriori was interesting and I'll certainly use it.

--- rProgramming/rMachineLearning/MLWithR/c08/CHapter8.R
+++ rProgramming/rMachineLearning/MLWithR/c08/CHapter8.R
@@ -21,3 +21,29 @@ library(arules)
 
 groceries <- read.transactions("groceries.csv", sep= ",")
 summary(groceries)
+
+
+
+
+image(sample(groceries, 100))
+
+groceryrules = apriori(groceries, parameter = list(support = .006, confidence = .25, minlen = 2))
+groceryrules
+
+summary(groceryrules)
+
+# This package is awesome; that I can use transactions basically as vectors is really clever.
+inspect(groceryrules[1:3])
+
+# arules has a sort function of its own.
+inspect( sort(groceryrules, by = "lift")[1:5] )
+
+berryrules <- subset(groceryrules, items %in% "berries")
+inspect(berryrules)
+
+# %ain% and %pin% are interesting too; a = all, p = partial
+# these are used in conjunction with subset()
+
+# I already know about write and I don't want to include it
+write(groceryrules, file = "groceryrules.csv", sep = ",", quote = TRUE, row.names = FALSE)
+
---
+++ rProgramming/rMachineLearning/MLWithR/c09/Chapter9.R
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------
+# This are my personal notes about the Text "Machine Learning with R"
+# by Brett Lantz. I'll be splitting up the notes by chapters.
+
+#-------------------------
+# Quick Notes:
+##- C8
+##- Apriori Principle
+##- Sparse Matrix
+##- arules package
+#----------
+# Chapter 9
